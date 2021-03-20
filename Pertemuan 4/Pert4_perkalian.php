7b2ff58f146be17c8385fae252d2d1c6d7d1c164
hidayatullah10A <hidayatullahdayat063@gmail.com>
2021-03-20 22:10:48 +0700

    Hidayatullah A

---
+++ Pertemuan 4/Pert4_perkalian.php
@@ -0,0 +1,25 @@
+<?php
+
+for($a=1;$a<=1;$a++)
+
+{
+
+for($b=15;$b<=100;$b++)
+
+{
+
+$b+=2;
+
+$jum=$a*$b;
+
+echo"<table>";
+
+echo"<tr><td>$a x $b</td><td>=$jum<br></td></tr>";
+
+}
+
+}
+
+echo"</table>";
+
+?>
